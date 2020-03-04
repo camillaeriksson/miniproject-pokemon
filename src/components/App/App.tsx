585412db8bed7d43ef347b2ffe36941d49a49945
Lisa <lisa.bengtsson@live.com>
2020-03-04 12:02:47 +0100

    working on hamburger

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 interface Props {
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,11 @@
 import React from "react"
-import './Header.css'
-interface State {
+import "./Header.css"
+import icon from "./assets/icons/pokemon.png"
+import Hambuger from "../Hamburger/Hamburger"
+import { BrowserRouter, Link } from "react-router-dom"
 
+interface State {
+isOpen: Boolean
 }
 interface Props {
 
@@ -10,15 +14,42 @@ class Header extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props)
 
+        this.state = {
+            isOpen: false
+        }
+
+    }
+
+    handleOnClick = () => {
+        this.setState({ isOpen: !this.state.isOpen })
     }
 
 
     render() {
-        return(
-            <div>
-                <h1>POKEPEDIA</h1>
-                <i className="fas fa-bars"></i>
-            </div>
+        return (
+            <BrowserRouter>
+           <header className="header">
+               <i onClick={ this.handleOnClick } className="fas fa-bars"></i>
+               <div className="linkDiv">
+                   <Link className="links" to="/">Red</Link>
+                   <Link  className="links" to="/">Blue</Link>
+                   <Link className="links" to="/">Green</Link>
+                   <Link className="links" to="/">Pink</Link>
+                   <Link className="links" to="/">Yellow</Link>
+               </div>
+           </header>
+            </BrowserRouter>
+            /* <div>
+                <div className="h1">
+                    <h1>POKEPEDIA</h1>
+                </div>
+                <div className="icon">
+                    <a href="">
+                        <img src={icon} alt="icon" />
+                    </a>
+                </div>
+            </div> */
+            
 
         )
     }
