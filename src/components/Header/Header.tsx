9f838fe1901d4faf2f92570216a7dab9a77c082f
Lisa <lisa.bengtsson@live.com>
2020-03-06 14:30:16 +0100

    route back to startpage working

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,18 @@
 import React from "react"
 import "./Header.css"
 import icon from "./pokemon.png"
-import { BrowserRouter as Router,
+import {
+    BrowserRouter as Router,
     Switch,
     Route,
-    Link } from "react-router-dom"
+    Link
+} from "react-router-dom"
 import CategoryPage from "../CategoryPage/CategoryPage"
+import StartPage from "../StartPage/StartPage"
+import App from "../App/App"
 
 interface State {
-isOpen: Boolean
+    isOpen: Boolean
 }
 interface Props {
 
@@ -19,44 +23,50 @@ class Header extends React.Component<Props, State> {
 
         this.state = {
             isOpen: false,
-            
+
         }
 
     }
 
     handleOnClick = () => {
         this.setState({ isOpen: !this.state.isOpen })
-    
+
     }
 
 
     render() {
         return (
             <Router>
-           <header className="header">
-               <div className="headerItems">
-               <i onClick={ this.handleOnClick } className="fas fa-bars"/>
-               <img className="pokemonTitle" src="https://fontmeme.com/permalink/200304/fe08af5a72abf719bf67178557262a8f.png"/* src="https://fontmeme.com/permalink/200304/5aafb35ab92ffed133148b50bae2903b.png" */ alt=""/>
-               <img className="pokemonIcon" src={icon} alt="icon" />
-               </div>
-                {this.state.isOpen && 
-                    <div className="linkDiv" style={{ height: "260px" }}>
-                        <Link className="links" to="/category">Red</Link>
-                        <Link className="links" to="/">Blue</Link>
-                        <Link className="links" to="/">Green</Link>
-                        <Link className="links" to="/">Pink</Link>
-                        <Link className="links" to="/">Yellow</Link>
-                    </div>}
+                <header className="header">
+                    <div className="headerItems">
+                        <i onClick={this.handleOnClick} className="fas fa-bars" />
+                        <Link to="/">
+                            <img className="pokemonTitle"
+                                src="https://fontmeme.com/permalink/200304/fe08af5a72abf719bf67178557262a8f.png"
+                                alt="Title" />
+                        </Link>
+                        <Link to="">
+                            <img className="pokemonIcon" src={icon} alt="icon" />
+                        </Link>
+                    </div>
+                    {this.state.isOpen &&
+                        <div className="linkDiv" style={{ height: "260px" }}>
+                            <Link className="links" to="/category">Red</Link>
+                            <Link className="links" to="/category">Blue</Link>
+                            <Link className="links" to="/category">Green</Link>
+                            <Link className="links" to="/category">Pink</Link>
+                            <Link className="links" to="/category">Yellow</Link>
+                        </div>}
 
-           <Switch>
-           <Route path="/category">
-            <CategoryPage />
-          </Route>
-           </Switch>
-           </header>
+                    <Switch>
+                        <Route path="/category">
+                            <CategoryPage />
+                        </Route>
+                    </Switch>
+                </header>
             </Router>
-            
-            
+
+
 
         )
     }
