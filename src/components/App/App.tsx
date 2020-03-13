b1b8d9fa62f28ad0202ee51e13f838d8146292b8
Lisa <lisa.bengtsson@live.com>
2020-03-13 09:30:59 +0100

    in progress

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import StartPage from "../StartPage/StartPage"
@@ -6,12 +6,23 @@ import {
   BrowserRouter,
   Switch,
   Route,
-  useParams
+  useParams,
+  useLocation
 } from "react-router-dom";
 import ErrorBoundary from "../Errorboundry/errorboundry";
 import CategoryPage from '../CategoryPage/CategoryPage';
 import FavouritePage from '../FavouritePage/FavouritePage';
 
+  const Color = () => {
+  const { pathname } = useLocation();
+  
+  return (
+    <CategoryPage>
+      <h1>Contact</h1>
+      <p>Current URL: {pathname}</p>
+    </CategoryPage>
+    )
+    };
 
 
 function App() {
