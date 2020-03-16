22caa9681be833d7627fc248291d208677b52194
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-03-16 14:45:24 +0100

    Fixed component on startpage

--- src/components/StartPageInfo/StartPageInfo.tsx
+++ src/components/StartPageInfo/StartPageInfo.tsx
@@ -2,6 +2,9 @@ import React from "react"
 import ErrorBoundary from "../Errorboundry/errorboundry"
 import "./StartPageInfo.css"
 
+import squirtle from "../../assets/squirtle.png"
+import blue_pokemons from "../../assets/blue_pokemons.png"
+
 interface Props { }
 
 interface State { }
@@ -12,6 +15,10 @@ export default class StartPageInfo extends React.Component<Props, State> {
         return (
             <ErrorBoundary>
                 <div className="info_container">
+                    <img className="squirtle_pic" src={squirtle}></img>
+                    <p className="blue_text"><b>Start adding</b> Squirtle and his blue friends to your pokeball!</p>
+                    <button className="bluePokemonsButton">To the blue pokemons</button>
+                    <img className="blue_pokemons" src={blue_pokemons}></img>
                 </div>
             </ErrorBoundary>
         )
